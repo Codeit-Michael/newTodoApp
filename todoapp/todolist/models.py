5cbebe9e01413e4c17401673f7e9dcdc098b82f6
Michael Maranan <michaelmaranan030@gmail.com>
2021-09-27 09:43:37 +0800

    add newlist functions

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class todo(models.Model):
+	name = models.CharField(max_length = 50)
+
+	def __str__(self):
+		return self.name
+
+class item(models.Model):
+	list = models.ForeignKey(todo, on_delete = models.CASCADE)
+	text = models.CharField(max_length = 200)
+	is_complete = models.BooleanField()
+
+	def __str__(self):
+		return self.name
--- todoapp/todolist/views.py
+++ todoapp/todolist/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import todo
+from django.http import HttpResponse
+from .forms import todoForm
 
 # Create your views here.
+def view(request):
+	mylist = todo.objects.all()
+	return HttpResponse(f'{mylist}')
+
+def createnew(request):
+	myForm = todoForm()
+	if request.method == 'POST':
+		myForm = todoForm(request.POST)
+		if myForm.is_valid():
+			newlist = myForm.cleaned_data['name']
+			listname = todo(name=newlist)
+			listname.save()
+			return redirect('view')
+
+	return render(request, 'todolist/new.html', {'form':myForm})
