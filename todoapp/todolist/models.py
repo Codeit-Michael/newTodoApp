5244077ccf6b05e2c76233690840959823041336
Michael Maranan <michaelmaranan030@gmail.com>
2021-10-09 22:02:14 +0800

    removed user items

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
 class todo(models.Model):
-	user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
 	name = models.CharField(max_length = 50, null=True)
 
 	def __str__(self):
